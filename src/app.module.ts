9a895e171372f68b9d88db61a2508b17094026d7
Alex <belohvostov.belohvostov@gmail.com>
2022-10-12 09:45:25 +0300

    track/comment schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TrackModule],
+  imports: [
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://Alex:alex2308@cluster0.iduhji2.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
 })
 export class AppModule {}
